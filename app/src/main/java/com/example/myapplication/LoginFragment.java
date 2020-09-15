7ee6bc96d8c2a34c495347352ca3dd9518057a24
Sai Ngaung Seng Hein <snsh22@gmail.com>
2020-09-15 22:12:52 +0900

    firebase authetication

--- app/src/main/java/com/example/myapplication/LoginFragment.java
+++ app/src/main/java/com/example/myapplication/LoginFragment.java
@@ -2,11 +2,21 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +33,11 @@ public class LoginFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    EditText et_email,et_password,et_name,et_repassword;
+    Button btnRegister;
+    FirebaseAuth fAuth;
+
 
     public LoginFragment() {
         // Required empty public constructor
@@ -53,6 +68,43 @@ public class LoginFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        et_email = (EditText) getView().findViewById(R.id.et_email);
+        et_password = (EditText) getView().findViewById(R.id.et_password);
+        et_name = (EditText)getView().findViewById(R.id.et_name);
+        et_repassword = (EditText)getView().findViewById(R.id.et_repassword);
+        fAuth = FirebaseAuth.getInstance();
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = et_email.getText().toString().trim();
+                String password = et_password.getText().toString().trim();
+                System.out.println(email);
+
+                if(TextUtils.isEmpty(email)){
+                    et_email.setText("Email is Required");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    et_password.setText("Password is Required");
+                    return;
+                }
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(getContext(), "User Created", Toast.LENGTH_SHORT).show();
+                        }else {
+                            Toast.makeText(getContext(), "Error!!!"+ task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+                });
+
+            }
+        });
+
     }
 
     @Override
